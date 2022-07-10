adcfeff84d2161f4c00ffb41cedb966baac3f770
Ken <ken_chief_engineer@yahoo.com>
2022-07-10 19:24:41 -0400

    Update title
    update to CSC299

--- csc299checkpoint/index.php
+++ csc299checkpoint/index.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous"
-		<title>ASE 230 - class of Fall 2021 Great Authors - Index Page</title>
+		<title CSC 299 - class of Summer 2022 Great Authors - Index Page</title>
 	</head>
 
 	<?php
